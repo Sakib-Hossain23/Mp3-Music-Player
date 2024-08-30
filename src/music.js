4f8113ac1a5e0241e8cf81f1a63726cf3ac98011
Md.Sakib Hossain <168235198+Sakib-Hossain23@users.noreply.github.com>
2024-08-31 01:00:26 +0600

    Add files via upload

---
+++ src/music.js
@@ -0,0 +1,44 @@
+let allMusic = [
+
+      {
+        name: "Apna Bana Le",
+        artist: "Arijit Singh",
+        img: "run",
+        src: "run"
+    },
+
+    {
+        name: "Phir Bhi Tumko Chahunga",
+        artist: "Arijit Singh",
+        img: "perfect",
+        src: "perfect"
+    },
+
+    /*{
+        name: "Infinity - Feel Something",
+        artist: "Jaymes Young",
+        img: "infinity",
+        src: "infinity"
+    },
+
+    {
+        name: "No Lie - Now Dance",
+        artist: "Sean Paul & Dua Lipa",
+        img: "nolie",
+        src: "nolie"
+    },
+
+    {
+        name: "Sunflower - Spider-Man: Into the Spider-Verse",
+        artist: "Post Malone & Swae Lee",
+        img: "sunflower",
+        src: "sunflower"
+    },
+
+    {
+        name: "Heat Waves - Dreamland",
+        artist: "Glass Animals",
+        img: "heatwaves",
+        src: "heatwaves"
+    },*/
+]
